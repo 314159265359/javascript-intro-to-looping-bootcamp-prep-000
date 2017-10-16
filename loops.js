95f4252978913e15f56447bae1fc4ecf25643465
Karen G <kgraves0701@gmail.com>
2017-10-16 18:06:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for (var i = 1; i < 26; i++) {
-    console.log(array + "I am " + i + " strange loops.")
+    console.log(array + "I am " + i + " strange loop${i===0 ? '' : 's'}.")
     //console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.")
   }
 }
